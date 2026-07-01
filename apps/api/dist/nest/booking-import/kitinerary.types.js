f38179896712eecd7b0461d933065e0bf38b9a5a
Gibcity <28833616+Gibcity@users.noreply.github.com>
2026-07-01 18:57:09 -0500

    chore: remove stale dist/ files from git index
    These were tracked before apps/{api,web}/dist/ were added to .gitignore (the strictNullChecks commit c0660823 staged them as deletes, but the change was never consolidated into a focused commit). Consolidating now: git rm --cached removes them from the index while leaving the files on disk.

--- apps/api/dist/nest/booking-import/kitinerary.types.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-/** KItinerary JSON-LD output types (schema.org subset) */
-Object.defineProperty(exports, "__esModule", { value: true });
--- apps/api/dist/services/avatarUrl.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.avatarUrl = avatarUrl;
-function avatarUrl(user) {
-    return user.avatar ? `/uploads/avatars/${user.avatar}` : null;
-}
